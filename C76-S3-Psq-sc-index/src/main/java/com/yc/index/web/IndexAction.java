68ed226aa93eedeff76c28509c66dee9646a643c
luckypsq <1139386771@qq.com>
2020-03-20 17:35:14 +0800

    熔断与降级服务

--- C76-S3-Psq-sc-index/src/main/java/com/yc/index/web/IndexAction.java
+++ C76-S3-Psq-sc-index/src/main/java/com/yc/index/web/IndexAction.java
@@ -1,20 +1,39 @@
 package com.yc.index.web;
 
 import javax.annotation.Resource;
-import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+
 @RestController
 public class IndexAction {
 	@Resource
 	private RestTemplate restTemplate;
-	
-	
+
+	/*
+	 * @GetMapping("user/way") public String index(HttpServletRequest request) {
+	 * return "user:"+request.getServerPort(); }
+	 */
+
+	@Resource
+	private IUserAction iua;
+	@GetMapping("fuser/way") public String fuser() {
+		return iua.user(); 
+	}
+	 
+
 	@GetMapping("user/way")
-	public String index(HttpServletRequest request) {
-		return "user:"+request.getServerPort();
+	@HystrixCommand(fallbackMethod = "wayHystirx")
+	public String user() {
+		String url = "http://sc-user/user/way";
+		String ret = restTemplate.getForObject(url, String.class);
+		return ret;
+	}
+
+	public String wayHystirx() {
+		return "服务器熔断，服务降级了";
 	}
 }
--- C76-S3-Psq-sc-user/src/main/java/com/yc/user/web/UserAction.java
+++ C76-S3-Psq-sc-user/src/main/java/com/yc/user/web/UserAction.java
@@ -1,13 +1,15 @@
 package com.yc.user.web;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class IndexAction {
-	
+public class UserAction {
 	@GetMapping("user/way")
-	public String index() {
-		return "user";
+	public String index(HttpServletRequest req) {
+		return "user: " + req.getServerPort();
 	}
 }
+
