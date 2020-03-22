f026d218f53066133644c9dbaed1d74f15c3b2fd
luckypsq <1139386771@qq.com>
2020-03-22 11:29:05 +0800

    mybatis逆向工程

---
+++ C76-S3-Psq-ebuy-common/src/main/java/com/yc/ebuy/EbuyCommonApplication.java
@@ -0,0 +1,13 @@
+package com.yc.ebuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbuyCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbuyCommonApplication.class, args);
+	}
+
+}
