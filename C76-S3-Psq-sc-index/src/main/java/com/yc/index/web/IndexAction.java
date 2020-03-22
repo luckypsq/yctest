542e462b0075a9dc5cd59e00a19cd79286681014
luckypsq <1139386771@qq.com>
2020-03-22 10:03:13 +0800

    易购网父工程搭建

--- C76-S3-Psq-sc-index/src/main/java/com/yc/index/web/IndexAction.java
+++ C76-S3-Psq-sc-index/src/main/java/com/yc/index/web/IndexAction.java
@@ -10,30 +10,35 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 
 @RestController
 public class IndexAction {
+	
 	@Resource
 	private RestTemplate restTemplate;
 
-	/*
-	 * @GetMapping("user/way") public String index(HttpServletRequest request) {
-	 * return "user:"+request.getServerPort(); }
-	 */
-
-	@Resource
-	private IUserAction iua;
-	@GetMapping("fuser/way") public String fuser() {
-		return iua.user(); 
+	@GetMapping("index/way")
+	public String index() {
+		return "index";
 	}
-	 
-
+	
 	@GetMapping("user/way")
-	@HystrixCommand(fallbackMethod = "wayHystirx")
+	@HystrixCommand(fallbackMethod="wayHystirx")
 	public String user() {
+		// 使用服务名替代 ip + 端口
 		String url = "http://sc-user/user/way";
+		// 后台远程调用
 		String ret = restTemplate.getForObject(url, String.class);
 		return ret;
 	}
-
+	
 	public String wayHystirx() {
-		return "服务器熔断，服务降级了";
+		return "服务器熔断, 服务降级了！！";
 	}
+	
+	@Resource
+	private IUserAction iua;
+	
+	@GetMapping("fuser/way")
+	public String fuser() {
+		return iua.user();
+	}
+	
 }
