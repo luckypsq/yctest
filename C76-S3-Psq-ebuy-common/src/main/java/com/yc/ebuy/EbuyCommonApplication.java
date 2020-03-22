a236f2f79e3a9fecbf8f797e3d0400adfe0486e0
luckypsq <1139386771@qq.com>
2020-03-22 12:31:27 +0800

    mybatis测试

--- C76-S3-Psq-ebuy-common/src/main/java/com/yc/ebuy/EbuyCommonApplication.java
+++ C76-S3-Psq-ebuy-common/src/main/java/com/yc/ebuy/EbuyCommonApplication.java
@@ -1,9 +1,12 @@
 package com.yc.ebuy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
+@MapperScan("com.yc.ebuy")
 public class EbuyCommonApplication {
 
 	public static void main(String[] args) {
---
+++ C76-S3-Psq-ebuy-common/src/test/java/com/yc/ebuy/EbuyApplicationTests.java
@@ -0,0 +1,22 @@
+package com.yc.ebuy;
+
+import javax.annotation.Resource;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.yc.ebuy.dao.EasybuyUserMapper;
+
+
+@SpringBootTest
+public class EbuyApplicationTests {
+
+	@Resource
+	EasybuyUserMapper um;
+
+	@Test
+	public void test2() {
+		System.out.println(um.selectByExample(null));
+	}
+
+}
